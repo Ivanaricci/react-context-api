13e20fe72160d8b43412a0e03f75bc0f6d09a214
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-19 16:59:07 +0200

    [UPD] create context

---
+++ src/Context.jsx
@@ -0,0 +1,8 @@
+// importo la funzione createContext
+import { createContext } from "react";
+
+// utilizzo la funzione per creare il contesto
+const PostsContext = CreateContext();
+
+// esportiamo il contesto
+export default PostsContext
