baaf3b12551c09845600c2dee9c5b17a1da01276
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-19 16:52:04 +0200

    [UPD] Create PostPage

---
+++ src/PostPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// importare postList (da creare)
+
+const PostPage = () => {
+    return (
+        <div>
+            <h1>Posts</h1>
+            {/* componente postList */}
+        </div>
+    )
+}
+
+export default PostPage
