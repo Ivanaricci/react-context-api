f2b18c07c794dd334703a87ae7dd722d49277e8f
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-19 17:11:54 +0200

    [UPD] import context in PostList

--- src/PostList.jsx
+++ src/PostList.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+// importo l'hook useContext
+import { useContext } from 'react'
+
+// importo il contesto che voglio utilizzare
+import PostsContext from './Context'
+
 const PostList = () => {
     return (
         <div>PostList</div>
